e93ba02c3f699f734664a03e195ceba93e72d5a5
YALLICO OWNER <yallico2024@gmail.com>
2025-07-04 03:25:40 +0200

    Update ash-stock2.js

--- plugins/ash-stock2.js
+++ plugins/ash-stock2.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
     if (text) {
-        global.db.data.chats[m.chat].setprecios = text
-        conn.reply(m.chat, '> Tu Lista a Sido Actualizada Correctamente Pon .precios Para Ver La Lista 🥖', m)
+        global.db.data.chats[m.chat].setstock2 = text
+        conn.reply(m.chat, '> Tu Lista a Sido Actualizada Correctamente Pon .stock2 Para Ver La Lista 🥖', m)
     } else throw `> Para Actualizar Tu Lista Debes Poner Lo Siguiente .setstock1 + Tu Lista\n> Ejemplo :\n.setstock2 1 bot = 5soles`
 }
 
