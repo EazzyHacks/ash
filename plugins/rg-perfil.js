6012808154804c1f643b7a5e8fe0b28e40052e35
EazzyHacks <yhair7200@gmail.com>
2025-08-04 09:52:54 +0200

    Update rg-perfil.js

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -2,7 +2,7 @@ import PhoneNumber from 'awesome-phonenumber';
 import fetch from 'node-fetch';
 import fs from 'fs';
 
-const imagen1 = 'https://qu.ax/cqUYc.jpg';
+const imagen1 = 'https://qu.ax/nXGTW.jpeg';
 
 const loadMarriages = () => {
     if (fs.existsSync('./media/database/marry.json')) {
